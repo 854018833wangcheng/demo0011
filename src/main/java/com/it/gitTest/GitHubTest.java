a8451a307fa3dfcfd3292da5a6b1ae78b95e82e0
854018833wangcheng <854018833@qq.com>
2022-05-13 19:12:17 +0800

    first commit main

---
+++ src/main/java/com/it/gitTest/GitHubTest.java
@@ -0,0 +1,11 @@
+package com.it.gitTest;
+
+/**
+ * @Author WangCheng
+ * @create 2022/5/13 19:10
+ */
+public class GitHubTest {
+    public static void main(String[] args) {
+        System.out.println("Test---mmaster");
+    }
+}
